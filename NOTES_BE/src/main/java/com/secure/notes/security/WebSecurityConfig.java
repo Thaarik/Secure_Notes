2d055319b1ee71a20093ea4e23cb5d6a244974bc
Thaarik Ahamed A <a.thaarikahamed@gmail.com>
2024-08-23 15:44:27 +0100

    in memory usage

--- NOTES_BE/src/main/java/com/secure/notes/security/WebSecurityConfig.java
+++ NOTES_BE/src/main/java/com/secure/notes/security/WebSecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,5 +32,29 @@ public class WebSecurityConfig {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(Customizer.withDefaults()); //basic authentication (not default in-built form based auth)
         return http.build();
+    }
+
+    // configuring multiple users in in-memory
+    // test by POST and GET request the notes using the below username and password
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager(); //setting up in-memory authentication
+        if(!manager.userExists("user1")){ // creates user1 if not exists
+            manager.createUser(
+                    User.withUsername("user1")
+                            .password("{noop}password1") //{noop} letting spring security know that this password is stored in plain text without any encoding.
+                            .roles("USER")
+                            .build()
+            );
+        }
+        if(!manager.userExists("admin")){ // creates admin if not exists
+            manager.createUser(
+                    User.withUsername("admin")
+                            .password("{noop}adminPass")
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+        return manager;
     }
 }
