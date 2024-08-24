36bfaf709ee61103c99e59da0d66e1d694ddfc2a
Thaarik Ahamed A <a.thaarikahamed@gmail.com>
2024-08-24 15:11:15 +0100

    Custom based user details

--- NOTES_BE/src/main/java/com/secure/notes/security/WebSecurityConfig.java
+++ NOTES_BE/src/main/java/com/secure/notes/security/WebSecurityConfig.java
@@ -1,6 +1,12 @@
 package com.secure.notes.security;
 
 
+import com.secure.notes.model.AppRole;
+import com.secure.notes.model.Role;
+import com.secure.notes.model.User;
+import com.secure.notes.repository.RoleRepository;
+import com.secure.notes.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,13 +14,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
+//import javax.sql.DataSource;
+import java.time.LocalDate;
 
 @Configuration
 @EnableWebSecurity
@@ -37,27 +43,66 @@ public class WebSecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public CommandLineRunner initData(RoleRepository roleRepository, UserRepository userRepository) {
+        return args -> {
+            Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                    .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_USER))); // if role exists, use that role, if not, create a new role
+
+            Role adminRole = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+                    .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_ADMIN))); // if role exists, use that role, if not, create a new role
+
+            if (!userRepository.existsByUserName("user1")) {
+                User user1 = new User("user1", "user1@example.com", "{noop}password1");
+                user1.setAccountNonLocked(false);
+                user1.setAccountNonExpired(true);
+                user1.setCredentialsNonExpired(true);
+                user1.setEnabled(true);
+                user1.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+                user1.setAccountExpiryDate(LocalDate.now().plusYears(1));
+                user1.setTwoFactorEnabled(false);
+                user1.setSignUpMethod("email");
+                user1.setRole(userRole);
+                userRepository.save(user1);
+            }
+
+            if (!userRepository.existsByUserName("admin")) {
+                User admin = new User("admin", "admin@example.com", "{noop}adminPass");
+                admin.setAccountNonLocked(true);
+                admin.setAccountNonExpired(true);
+                admin.setCredentialsNonExpired(true);
+                admin.setEnabled(true);
+                admin.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+                admin.setAccountExpiryDate(LocalDate.now().plusYears(1));
+                admin.setTwoFactorEnabled(false);
+                admin.setSignUpMethod("email");
+                admin.setRole(adminRole);
+                userRepository.save(admin);
+            }
+        };
+    }
     // configuring multiple users in JDBC
     // test by POST and GET request the notes using the below username and password
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource); //setting up jdbc authentication
-        if(!manager.userExists("user1")){ // creates user1 if not exists
-            manager.createUser(
-                    User.withUsername("user1")
-                            .password("{noop}password1") //{noop} letting spring security know that this password is stored in plain text without any encoding.
-                            .roles("USER")
-                            .build()
-            );
-        }
-        if(!manager.userExists("admin")){ // creates admin if not exists
-            manager.createUser(
-                    User.withUsername("admin")
-                            .password("{noop}adminPass")
-                            .roles("ADMIN")
-                            .build()
-            );
-        }
-        return manager;
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource); //setting up jdbc authentication
+//        // default expirations are provided in JdbcUserDetailsManager
+//        if(!manager.userExists("user1")){ // creates user1 if not exists
+//            manager.createUser(
+//                    User.withUsername("user1")
+//                            .password("{noop}password1") //{noop} letting spring security know that this password is stored in plain text without any encoding.
+//                            .roles("USER")
+//                            .build()
+//            );
+//        }
+//        if(!manager.userExists("admin")){ // creates admin if not exists
+//            manager.createUser(
+//                    User.withUsername("admin")
+//                            .password("{noop}adminPass")
+//                            .roles("ADMIN")
+//                            .build()
+//            );
+//        }
+//        return manager;
+//    }
 }
